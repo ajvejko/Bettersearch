360b7584d7dc25b470e1b0fcb307650f1bff6688
ajvejko <moucka.matej01@gmail.com>
2023-10-01 00:04:02 +0200

    fixed border of selected buttons

--- src/components/WebButton.vue
+++ src/components/WebButton.vue
@@ -8,9 +8,7 @@ import { windowStore } from "@/stores/windowStores";
   <button
     class="dark:bg-dark/5 mr-2 mt-2 rounded-xl border-2 border-black px-2 pb-0.5 font-inter font-bold text-textLight shadow-[0_0px_5px_rgba(0,0,0,0.25)] hover:bg-white/10 active:shadow-black dark:border-white dark:text-textDark dark:hover:bg-black/10 dark:active:shadow-white md:text-lg"
     :class="[
-      buttonStore.name === name
-        ? 'border-primary dark:border-primary'
-        : '',
+      buttonStore.name === name ? '!dark:border-primary !border-primary' : '',
       windowStore.EditMode ? '!dark:text-primary !text-primary' : '',
     ]"
   >
